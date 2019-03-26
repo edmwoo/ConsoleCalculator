feef59828c1fbef63e9025d66f87798182918d23
Ed Woo <edmwoo@gmail.com>
2019-03-25 22:24:13 -0700

    Add Unit Test to ConsoleCalculator

---
+++ ConsoleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+
+            double result = _calculatorEngine.Calculate(number1, number2, "add");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+
+            double result = _calculatorEngine.Calculate(number1, number2, "+");
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ ConsoleCalculator.Test.Unit/StringToNumericTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleCalculator.Test.Unit
+{
+    [TestClass]
+    public class StringToNumericTest
+    {
+        private readonly StringToNumeric _stringToNumeric = new StringToNumeric();
+        [TestMethod]
+        public void ConvertValidStringInputToDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _stringToNumeric.GetNumber(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertInvalidStringToNumber()
+        {
+            string inputNumber = "not_num";
+            double convertedNumber = _stringToNumeric.GetNumber(inputNumber);
+        }
+
+    }
+}
